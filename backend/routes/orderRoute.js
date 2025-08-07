0b4ad247dab31260dc776c1561cada01f67aabf7
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2025-08-07 09:52:37 +0530

    commit cc

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,7 +2,6 @@ import express from 'express'
 import authMiddleware from '../middleware/auth.js'
 import { placeOrder, userOrder, verifyOrder, listOrders, updateStatus, codOrder } from '../controllers/orderController.js'
 
-
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
@@ -11,7 +10,6 @@ orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrder);
 orderRouter.get("/list", listOrders)
 orderRouter.post("/status", updateStatus);
-
 
 
 export default orderRouter
